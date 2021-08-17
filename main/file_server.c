403e3255d971dd0c50d01bfe96195e89175a87b2
holmatic <holmscott@gmx.de>
2021-08-17 22:12:20 +0200

    cleanup

--- main/file_server.c
+++ main/file_server.c
@@ -25,7 +25,6 @@
 #include "esp_spiffs.h"
 #include "esp_http_server.h"
 
-#include "signal_to_zx.h"
 
 /* Max length a file path can have on storage */
 #define FILE_PATH_MAX (ESP_VFS_PATH_MAX + CONFIG_SPIFFS_OBJ_NAME_LEN)
